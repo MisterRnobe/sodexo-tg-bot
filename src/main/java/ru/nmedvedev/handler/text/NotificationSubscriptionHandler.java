c9b14310e29ab8f3c1b4950b864c57f5cc671666
MisterRnobe <mr.rnobe@gmail.com>
2020-10-10 21:48:57 +0300

    Save current latest operation when subdcribed

--- src/main/java/ru/nmedvedev/handler/text/NotificationSubscriptionHandler.java
+++ src/main/java/ru/nmedvedev/handler/text/NotificationSubscriptionHandler.java
@@ -1,21 +1,33 @@
 package ru.nmedvedev.handler.text;
 
 import io.smallrye.mutiny.Uni;
-import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import ru.nmedvedev.handler.InputTextHandler;
+import ru.nmedvedev.model.HistoryDb;
+import ru.nmedvedev.model.SodexoData;
+import ru.nmedvedev.model.SodexoResponse;
 import ru.nmedvedev.model.UserDb;
 import ru.nmedvedev.repository.UserRepository;
+import ru.nmedvedev.rest.SodexoClient;
 import ru.nmedvedev.view.ReplyButtonsProvider;
 import ru.nmedvedev.view.Response;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@RequiredArgsConstructor
 public class NotificationSubscriptionHandler implements InputTextHandler {
 
     private final UserRepository userRepository;
     private final ReplyButtonsProvider replyButtonsProvider;
+    private final SodexoClient sodexoClient;
+
+    public NotificationSubscriptionHandler(UserRepository userRepository,
+                                           ReplyButtonsProvider replyButtonsProvider,
+                                           @RestClient SodexoClient sodexoClient) {
+        this.userRepository = userRepository;
+        this.replyButtonsProvider = replyButtonsProvider;
+        this.sodexoClient = sodexoClient;
+    }
 
     @Override
     public String getPattern() {
@@ -34,7 +46,29 @@ public class NotificationSubscriptionHandler implements InputTextHandler {
         var message = userDb.getSubscribed()
                 ? "Теперь я вам буду сообщать о всех зачислениям и списаниях"
                 : "Теперь я вам не буду сообщать о всех зачислениям и списаниях";
-        return userRepository.persistOrUpdate(userDb)
+        return Uni.createFrom().item(userDb)
+                .onItem().transformToUni(this::updateLatestOperation)
+                .invokeUni(userRepository::persistOrUpdate)
                 .map(v -> Response.withReplyButtons(message, replyButtonsProvider.provideMenuButtons()));
+    }
+
+    private Uni<? extends UserDb> updateLatestOperation(UserDb userDb) {
+        if (userDb.getSubscribed()) {
+            return sodexoClient.getByCard(userDb.getCard())
+                    .map(SodexoResponse::getData)
+                    .map(SodexoData::getHistory)
+                    .map(list -> list.get(0))
+                    .map(latest -> new HistoryDb(latest.getAmount(), latest.getCurrency(), latest.getLocationName().get(0), latest.getTime()))
+                    .map(historyDb -> UserDb
+                            .builder()
+                            .id(userDb.id)
+                            .card(userDb.getCard())
+                            .chatId(userDb.getChatId())
+                            .subscribed(userDb.getSubscribed())
+                            .latestOperation(historyDb)
+                            .build());
+        } else {
+            return Uni.createFrom().item(userDb);
+        }
     }
 }
