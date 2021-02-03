4274b175cd8c66739319c535676cfef24896eb2f
Pavel Yurkin <woodythegreat@yandex.ru>
2021-02-03 19:51:41 +0300

    #26 tests passed on Windows

--- src/test/java/ru/nmedvedev/Helper.java
+++ src/test/java/ru/nmedvedev/Helper.java
@@ -1,11 +1,16 @@
 package ru.nmedvedev;
 
+import java.util.Locale;
 import java.util.Random;
 
 public final class Helper {
 
     public static final String CARD = String.valueOf(Math.abs(new Random().nextLong()));
     public static final Long CHAT = new Random().nextLong();
+
+    static {
+        Locale.setDefault(Locale.ROOT);
+    }
 
     private Helper() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
