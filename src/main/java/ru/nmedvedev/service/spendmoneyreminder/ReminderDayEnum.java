00d5083b6b2207823b2d5ecc2033279d39e4c96d
Pavel Yurkin <woodythegreat@yandex.ru>
2021-02-03 23:11:28 +0300

    #26 add SpendMoneyReminderService

---
+++ src/main/java/ru/nmedvedev/service/spendmoneyreminder/ReminderDayEnum.java
@@ -0,0 +1,15 @@
+package ru.nmedvedev.service.spendmoneyreminder;
+
+public enum ReminderDayEnum {
+    NOT_A_DAY_FOR_A_REMINDER(""),
+    HALF_MONTH_REMINDER("Дружок. Половина месяца прошло. У тебя осталось %.2f руб. Не забудь потратить)"),
+    LAST_WORKING_DAY_MINUS_THREE("На твоём счету %.2f руб. Через 3 дня они сгорят."),
+    LAST_WORKING_DAY_MINUS_ONE("Предпоследний день, чтобы потратить свои %.2f руб."),
+    LAST_WORKING_DAY("Ахтунг, комрад! У тебя ещё %.2f руб. на карте, а сегодня последний день чтобы их потратить!!!");
+
+    public final String messageFormat;
+
+    ReminderDayEnum(String messageFormat) {
+        this.messageFormat = messageFormat;
+    }
+}
---
+++ src/main/java/ru/nmedvedev/service/spendmoneyreminder/SpendMoneyReminderService.java
@@ -0,0 +1,49 @@
+package ru.nmedvedev.service.spendmoneyreminder;
+
+import io.quarkus.scheduler.Scheduled;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ru.nmedvedev.model.HistoryDb;
+import ru.nmedvedev.repository.UserRepository;
+import ru.nmedvedev.service.TelegramService;
+import ru.nmedvedev.view.ReplyButtonsProvider;
+import ru.nmedvedev.view.Response;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
+public class SpendMoneyReminderService {
+
+    private final RemindDayProviderService remindDayProviderService;
+    private final SpendMoneyReminderBusinessLogicGateService spendMoneyReminderBusinessLogicGateService;
+    private final UserRepository userRepository;
+    private final TelegramService telegramService;
+    private final ReplyButtonsProvider replyButtonsProvider;
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void sendReminders() {
+        ReminderDayEnum day = remindDayProviderService.getDay(LocalDate.now());
+        if (day == ReminderDayEnum.NOT_A_DAY_FOR_A_REMINDER) {
+            return;
+        }
+
+        userRepository.findSubscribedToSpendMoneyReminderWithCardAndChat()
+                .filter(user -> Optional.ofNullable(user.getLatestOperation()).map(HistoryDb::getAmount)
+                        .map(amountt -> spendMoneyReminderBusinessLogicGateService.needToSendNotification(day, amountt))
+                        .orElse(false))
+                .invoke(user -> telegramService.sendMessage(user.getChatId(), Response.withReplyButtons(
+                        String.format(day.messageFormat, user.getLatestOperation().getAmount()),
+                        replyButtonsProvider.provideMenuButtons()
+                )))
+                .onFailure()
+                .retry()
+                .atMost(5)
+                .subscribe()
+                .with(user -> log.info("Sent notification for user {} to chat {} with amount {}",
+                        user.getId(), user.getChatId(), user.getLatestOperation().getAmount()));
+    }
+}
