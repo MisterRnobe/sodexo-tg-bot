522ce89bc7027dfbccd61ecbb8f2afbbbe9f30c4
Pavel Yurkin <pavel.yurkin@leroymerlin.ru>
2021-02-07 14:35:35 +0300

    #26 remove unused

--- src/main/java/ru/nmedvedev/model/UserDb.java
+++ src/main/java/ru/nmedvedev/model/UserDb.java
@@ -1,9 +1,11 @@
 package ru.nmedvedev.model;
 
 import io.quarkus.mongodb.panache.MongoEntity;
-import io.quarkus.mongodb.panache.PanacheMongoEntity;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
 
@@ -20,16 +22,6 @@ public class UserDb {
     private String card;
     private Boolean subscribed = false;
     private boolean subscribedToSpendMoneyReminder = false;
-    private HistoryDb latestOperation = null;
+    private HistoryDb latestOperation;
 
-    @Deprecated(forRemoval = true)
-    public static UserDb withId(ObjectId id,
-                                Long chatId,
-                                String card,
-                                Boolean subscribed,
-                                Boolean subscribedToSpendMoneyReminder) {
-        var userDb = new UserDb(id, chatId, card, subscribed, subscribedToSpendMoneyReminder, null);
-        userDb.id = id;
-        return userDb;
-    }
 }
